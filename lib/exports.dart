87b392711d8e463e23afb1b3193456fb3d227dd2
Tiancris <tiancris01@gmail.com>
2024-01-25 13:31:45 -0500

    feat: exports file and  todo_app file created

---
+++ lib/exports.dart
@@ -0,0 +1,3 @@
+export 'package:flutter/material.dart';
+
+export 'package:todo_app/todo_app.dart';
---
+++ lib/todo_app.dart
@@ -0,0 +1,13 @@
+import 'exports.dart';
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo List',
+      home: Container(),
+    );
+  }
+}
